1cdb40fcc25525371b74e446a3846ef1137dcd15
Novatruepower <patatotrape3@gmail.com>
2024-03-22 11:33:45 -0400

    not done

--- NoSql-Brosseau_Mathis_Lepage_Vincent/Form1.cs
+++ NoSql-Brosseau_Mathis_Lepage_Vincent/Form1.cs
@@ -8,15 +8,26 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using MongoDB.Driver;
+using MongoDB.Bson;
+using NoSql_Brosseau_Mathis_Lepage_Vincent.Properties;
 
 namespace NoSql_Brosseau_Mathis_Lepage_Vincent
 {
     public partial class Form1 : Form
     {
-        SqlConnection connSql = new SqlConnection("mongodb://localhost:27017");
+        const string connectionUri = "mongodb://localhost:27017";
+        MongoClient client = new MongoClient(connectionUri);
+        IMongoDatabase db;
+
 
         public Form1()
         {
+            IMongoDatabase db = client.GetDatabase("exemple");
+            var joueurs = db.GetCollection<BsonDocument>("Joueurs");
+            var filter = Builders<BsonDocument>.Filter.Eq("_id", 2);
+            Console.WriteLine(joueurs.Find(filter));
+
             InitializeComponent();
         }
     }
